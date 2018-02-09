f13c04d106a55e35ed5f0cadc02a1ef02896abc5
m25120877 <31494582+m25120877@users.noreply.github.com>
2018-02-09 15:48:31 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,58 @@
 var dropzone;
+//var img;
+var imgArray=new Array();
+var isok;
+var imgnumber = 0;
+
+function preload() {
+    imgArray.push(loadImage('YOUWEREHERE_02.jpg'));
+    imgArray.push(loadImage('20180201_153714.jpg'));
+    imgArray.push(loadImage('k.jpg'));
+    imgArray.push(loadImage('IMG_2426.jpg'));
+    
+}
+
 function setup() {
   // put setup code here
-    createCanvas(500,500);
+    createCanvas(800,600);
     background(100,200,200);
 
     
     dropzone =select('#dropzone');
     dropzone.dragOver(highlight);
     dropzone.dragLeave(unhighlight);
+    dropzone.drop(getFile,unhighlight);
+    isok = false;
+}
 
+function getFile(file){
+    console.log(file.name);
+    if (file.name=='YOUWEREHERE_02.jpg'){
+        console.log('yes!');
+        isok = true;
+        imgnumber = 0;
+    } else if (file.name=='20180201_153714.jpg'){
+        console.log('yes!');
+        isok = true;
+        imgnumber = 1;
+    } else if (file.name=='k.jpg'){
+        console.log('yes!');
+        isok = true;
+        imgnumber =2;
+    } else if (file.name=='IMG_2426.jpg'){
+        console.log('yes!');
+        isok = true;
+        imgnumber = 3;
+    }
+        
+        
+           
 }
  
     
 
 function highlight(){
-    dropzone.style('background-color','#ccc');
+    dropzone.style('background-color','#DAA520');
     
 }
 function unhighlight(){
@@ -22,5 +60,12 @@ function unhighlight(){
 }
 
 function draw() {
+    textSize(40);
+    text("Voyager converter",50,80);
+    textSize(18);
+    text("Just for You Were Here",50,130);
   // put drawing code here
+    if (isok) {
+        image(imgArray[imgnumber],0,0,800,600);
+    }
 }
